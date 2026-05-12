6b55864fc32e96b20102702bd5bad6393711dbc3
jpuig3-crypto <jpuig3@uoc.edu>
2026-05-12 21:40:36 +0300

    actualizacion completada

---
+++ U2/U2EJ1 DOM access.js
@@ -0,0 +1,6 @@
+const myCircle = document.getElementById("circle");
+const myRect = document.getElementsByClassName("rect") [0];
+const myPolygon = document.getElementsByTagName("polygon") [0];
+
+
+console.log(myCircle, myRect, myPolygon);
